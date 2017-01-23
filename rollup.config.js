617dc5350f336bb4633a62310a4aacc573722871
Robin Houston <robin.houston@gmail.com>
2017-01-23 16:47:39 +0000

    No need for a “full” build, because we have no dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,10 @@
-var path = require("path"),
-    nodeResolve = require("rollup-plugin-node-resolve");
-
-// See the comment in src/slider.js
-const d3_scale_linear = path.resolve("./node_modules/d3-scale/src/linear.js");
+var nodeResolve = require("rollup-plugin-node-resolve");
 
 export default {
   entry: "src/popup.js",
   format: "umd",
   moduleName: "Popup",
   dest: "popup.js",
-
-  external: [ "d3-selection", "d3-axis", "d3-scale", d3_scale_linear ],
-  globals: { "d3-selection": "d3", "d3-axis": "d3", [d3_scale_linear]: "d3.scaleLinear" },
 
   plugins: [
     nodeResolve({jsnext: true})
