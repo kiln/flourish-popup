452886ea00e1b8d1c64b6b11dcc230d85a154b9a
Robin Houston <robin.houston@gmail.com>
2019-01-14 16:35:45 +0000

    Stop popups from making the page scrollable
    The popup element can technically be partly outside the page, even though visually it is not. This causes the page to become horizontally scrollable, which can lead to problems like kiln/flourish#1267.
    
    This commit adds an absolutely-positioned “constrainer” element containing the popup that has overlow:hidden, which covers the entire document. It’s surprisingly hard to achieve that – or at least I failed to find an easy way – and so we explicitly set the dimensions of the constrainer to match the document, and recompute them on window resize.
    
    Fixes #16.

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+# 1.2.1
+
+* Prevent popups at the edge of the document from making the document scrollable. #16
--- src/element.js
+++ src/element.js
@@ -67,7 +67,25 @@ export default function Popup__getElement() {
 		s.padding = "10px";
 		el.appendChild(content);
 
-		document.body.appendChild(el);
+		var constrainer = document.createElement("div");
+		s = constrainer.style;
+		s.overflow = "hidden";
+		s.pointerEvents = "none";
+		s.position = "absolute";
+		s.left = "0";
+		s.top = "0";
+		s.margin = "0";
+		s.padding = "0";
+
+		function resizeConstrainer() {
+			s.width = document.documentElement.scrollWidth + "px";
+			s.height = document.documentElement.scrollHeight + "px";
+		}
+		resizeConstrainer();
+		window.addEventListener("resize", resizeConstrainer);
+		constrainer.appendChild(el);
+
+		document.body.appendChild(constrainer);
 	}
 
 	return el;
